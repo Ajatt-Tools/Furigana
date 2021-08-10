7e6b207a1c8f643eee4a1ea49936a6c6791368e8
Ren Tatsumoto <tatsu@autistici.org>
2021-08-10 20:13:40 +0300

    update package.sh

--- package.sh
+++ package.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 readonly ROOT_DIR=$(git rev-parse --show-toplevel)
 readonly BRANCH=$(git branch --show-current)
 readonly ARCHIVE=japanese_support_$BRANCH.ankiaddon
+readonly MANIFEST='manifest.json'
 
 cd -- "$ROOT_DIR" || exit 1
 
@@ -13,3 +14,18 @@ git archive HEAD --format=zip --output "$ARCHIVE"
 git submodule foreach 'git archive HEAD --prefix=$path/ --format=zip --output "$ROOT_DIR/${path}_${BRANCH}.zip"'
 zipmerge "$ARCHIVE" ./*.zip
 rm -- ./*.zip
+
+if [[ $* ]]; then
+    # https://addon-docs.ankiweb.net/#/sharing?id=sharing-outside-ankiweb
+    # If you wish to distribute .ankiaddon files outside of AnkiWeb,
+    # your add-on folder needs to contain a ‘manifest.json’ file.
+    {
+        echo '{'
+        echo -e "\t\"package\": \"ajt_japanese_support\","
+        echo -e "\t\"name\": \"AJT Japanese support\","
+        echo -e "\t\"mod\": $(date -u '+%s')"
+        echo '}'
+    } > "$MANIFEST"
+    zip -u "$ARCHIVE" "$MANIFEST"
+    rm -- "$MANIFEST"
+fi
