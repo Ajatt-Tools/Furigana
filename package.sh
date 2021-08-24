801b26adbf2fa6b58a63adaea0635418e941ccfd
Ren Tatsumoto <tatsu@autistici.org>
2021-08-24 18:37:24 +0300

    update package.sh

--- package.sh
+++ package.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+readonly ADDON_NAME=ajt_furigana
 readonly ROOT_DIR=$(git rev-parse --show-toplevel)
-readonly BRANCH=$(git branch --show-current)
-readonly ARCHIVE=ajt_furigana_${BRANCH}.ankiaddon
+readonly BRANCH=${1:-$(git branch --show-current)}
+readonly ZIP_NAME=${ADDON_NAME}_${BRANCH}.ankiaddon
 
 cd -- "$ROOT_DIR" || exit 1
 
 export ROOT_DIR BRANCH
 
-git archive HEAD --format=zip --output "$ARCHIVE"
+git archive "$BRANCH" --format=zip --output "$ZIP_NAME"
+
 # shellcheck disable=SC2016
 git submodule foreach 'git archive HEAD --prefix=$path/ --format=zip --output "$ROOT_DIR/${path}_${BRANCH}.zip"'
-zipmerge "$ARCHIVE" ./*.zip
+
+zipmerge "$ZIP_NAME" ./*.zip
 rm -- ./*.zip
