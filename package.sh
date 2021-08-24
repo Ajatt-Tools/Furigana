3b5273d28fd8847cf9fc950804522cc69969f567
Ren Tatsumoto <tatsu@autistici.org>
2021-08-24 13:11:58 +0300

    remove manifest generation

--- package.sh
+++ package.sh
@@ -2,8 +2,7 @@
 
 readonly ROOT_DIR=$(git rev-parse --show-toplevel)
 readonly BRANCH=$(git branch --show-current)
-readonly ARCHIVE=japanese_support_$BRANCH.ankiaddon
-readonly MANIFEST='manifest.json'
+readonly ARCHIVE=ajt_furigana_${BRANCH}.ankiaddon
 
 cd -- "$ROOT_DIR" || exit 1
 
@@ -14,18 +13,3 @@ git archive HEAD --format=zip --output "$ARCHIVE"
 git submodule foreach 'git archive HEAD --prefix=$path/ --format=zip --output "$ROOT_DIR/${path}_${BRANCH}.zip"'
 zipmerge "$ARCHIVE" ./*.zip
 rm -- ./*.zip
-
-if [[ $* ]]; then
-    # https://addon-docs.ankiweb.net/#/sharing?id=sharing-outside-ankiweb
-    # If you wish to distribute .ankiaddon files outside of AnkiWeb,
-    # your add-on folder needs to contain a ‘manifest.json’ file.
-    {
-        echo '{'
-        echo -e "\t\"package\": \"ajt_japanese_support\","
-        echo -e "\t\"name\": \"AJT Japanese support\","
-        echo -e "\t\"mod\": $(date -u '+%s')"
-        echo '}'
-    } > "$MANIFEST"
-    zip -u "$ARCHIVE" "$MANIFEST"
-    rm -- "$MANIFEST"
-fi
